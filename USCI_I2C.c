27fafea1e969891fd266da76889e1be06089ed8c
Бибеа Андрей Романович <bibea_ar@tcsb.ru>
2019-12-09 11:55:19 +0300

    some visual improvements

--- USCI_I2C.c
+++ USCI_I2C.c
@@ -1,42 +1,40 @@
-//#include <msp430.h>
-//#include "cc430f5137.h"
 #include "cc430x513x.h"
 #include "USCI_I2C.h"
 
 
 void USCI_I2C_Init(void) {
-	PMAPPWD = 0x02D52;                        	// Get write-access to port mapping registers
-	P1MAP3 = PM_UCB0SDA;                      	// Map UCB0SDA output to P1.3
-	P1MAP2 = PM_UCB0SCL;                      	// Map UCB0SCL output to P1.2
-	PMAPPWD = 4;                              	// Lock port mapping registers
+	PMAPPWD = 0x02D52;							// Get write-access to port mapping registers
+	P1MAP3 = PM_UCB0SDA;						// Map UCB0SDA output to P1.3
+	P1MAP2 = PM_UCB0SCL;						// Map UCB0SCL output to P1.2
+	PMAPPWD = 4;								// Lock port mapping registers
 }
 
 
 void USCI_I2C_RX_Init(unsigned char slaveAddr, unsigned char prescale) {
-	P1SEL |= BIT2 + BIT3;                    	// Select P1.2 & P1.3 to I2C function
-	UCB0CTL1 |= UCSWRST;                      	// Enable SW reset
-	UCB0CTL0 = UCMST + UCMODE_3 + UCSYNC;     	// I2C Master, synchronous mode
-	UCB0CTL1 = UCSSEL_2 + UCSWRST;            	// Use SMCLK, keep SW reset
-	UCB0BR0 = prescale;        					// fSCL = SMCLK/prescale
+	P1SEL |= BIT2 + BIT3;						// Select P1.2 & P1.3 to I2C function
+	UCB0CTL1 |= UCSWRST;						// Enable SW reset
+	UCB0CTL0 = UCMST + UCMODE_3 + UCSYNC;		// I2C Master, synchronous mode
+	UCB0CTL1 = UCSSEL_2 + UCSWRST;				// Use SMCLK, keep SW reset
+	UCB0BR0 = prescale;							// fSCL = SMCLK/prescale
 	UCB0BR1 = 0;
-	UCB0I2CSA = slaveAddr;                      // Load slave address
-	UCB0CTL1 &= ~UCSWRST;                      	// Disable SW reset
-	UCB0IE |= UCNACKIE;                      	// Enable NACK interrupt
-	UCB0IE |= UCRXIE;                         	// Enable RX interrupt
+	UCB0I2CSA = slaveAddr;						// Load slave address
+	UCB0CTL1 &= ~UCSWRST;						// Disable SW reset
+	UCB0IE |= UCNACKIE;							// Enable NACK interrupt
+	UCB0IE |= UCRXIE;							// Enable RX interrupt
 }
 
 
 void USCI_I2C_TX_Init(unsigned char slaveAddr, unsigned char prescale) {
-	P1SEL |= BIT2 + BIT3;                    	// Select P1.2 & P1.3 to I2C function
-	UCB0CTL1 |= UCSWRST;                      	// Enable SW reset
-	UCB0CTL0 = UCMST + UCMODE_3 + UCSYNC;     	// I2C Master, synchronous mode
-	UCB0CTL1 = UCSSEL_2 + UCSWRST;            	// Use SMCLK, keep SW reset
-	UCB0BR0 = prescale;        					// fSCL = SMCLK/prescale
+	P1SEL |= BIT2 + BIT3;						// Select P1.2 & P1.3 to I2C function
+	UCB0CTL1 |= UCSWRST;						// Enable SW reset
+	UCB0CTL0 = UCMST + UCMODE_3 + UCSYNC;		// I2C Master, synchronous mode
+	UCB0CTL1 = UCSSEL_2 + UCSWRST;				// Use SMCLK, keep SW reset
+	UCB0BR0 = prescale;							// fSCL = SMCLK/prescale
 	UCB0BR1 = 0;
-	UCB0I2CSA = slaveAddr;                      // Load slave address
-	UCB0CTL1 &= ~UCSWRST;                      	// Disable SW reset
-	UCB0IE |= UCNACKIE;                      	// Enable NACK interrupt
-	UCB0IE |= UCTXIE;                         	// Enable TX interrupt
+	UCB0I2CSA = slaveAddr;						// Load slave address
+	UCB0CTL1 &= ~UCSWRST;						// Disable SW reset
+	UCB0IE |= UCNACKIE;							// Enable NACK interrupt
+	UCB0IE |= UCTXIE;							// Enable TX interrupt
 }
 
 
@@ -45,19 +43,19 @@ void USCI_I2C_RX(unsigned char RXdataCtr, unsigned char *RXdataPtr) {
 	if (RXdataCtr == 1) {
 		USCI_I2C_ctr_RX = 0;					// Load data counter
 		__disable_interrupt();					// GI disable while send conditions
-		UCB0CTL1 |= UCTXSTT;                   	// I2C start condition
-		while (UCB0CTL1 & UCTXSTT);             // Start condition sent?
-		UCB0CTL1 |= UCTXSTP;                    // I2C stop condition
+		UCB0CTL1 |= UCTXSTT;					// I2C start condition
+		while (UCB0CTL1 & UCTXSTT);				// Start condition sent?
+		UCB0CTL1 |= UCTXSTP;					// I2C stop condition
 		__enable_interrupt();					// GI enable again
-		__bis_SR_register(LPM0_bits + GIE);     // Enter LPM0, enable interrupts
+		__bis_SR_register(LPM0_bits + GIE);		// Enter LPM0, enable interrupts
 	}
 	else if (RXdataCtr > 1) {
 		USCI_I2C_ctr_RX = RXdataCtr - 2;		// Load data counter
-		UCB0CTL1 |= UCTXSTT;                    // I2C start condition
-		__bis_SR_register(LPM0_bits + GIE);     // Enter LPM0, enable interrupts
+		UCB0CTL1 |= UCTXSTT;					// I2C start condition
+		__bis_SR_register(LPM0_bits + GIE);		// Enter LPM0, enable interrupts
 	}
 	else {
-		while(1);								// illegal parameter
+		while(1);								// Trap if illegal parameter
 	}
 }
 
@@ -65,9 +63,9 @@ void USCI_I2C_RX(unsigned char RXdataCtr, unsigned char *RXdataPtr) {
 void USCI_I2C_TX(unsigned char TXdataCtr, unsigned char *TXdataPtr) {
 	USCI_I2C_ptr_TX = TXdataPtr;				// Load data pointer
 	USCI_I2C_ctr_TX = TXdataCtr;				// Load data counter
-	//UCB0CTL1 |= UCTXSTT;                    	// I2C start condition
-	UCB0CTL1 |= UCTR + UCTXSTT;             	// I2C TX, start condition
-	__bis_SR_register(LPM0_bits + GIE);     	// Enter LPM0, enable interrupts
+	//UCB0CTL1 |= UCTXSTT;						// I2C start condition
+	UCB0CTL1 |= UCTR + UCTXSTT;					// I2C TX, start condition
+	__bis_SR_register(LPM0_bits + GIE);			// Enter LPM0, enable interrupts
 }
 
 
@@ -93,7 +91,7 @@ void __attribute__ ((interrupt(USCI_B0_VECTOR))) USCI_B0_ISR (void)
 		case  4:															// Vector  4: NACKIFG
 			// TODO delivering status check
 			//USCI_I2C_ACKed = 0;
-			UCB0CTL1 |= UCTXSTP;                    						// I2C stop condition
+			UCB0CTL1 |= UCTXSTP;											// I2C stop condition
 			UCB0IFG &= ~UCNACKIFG;											// Clear USCI_B0 NACK interrupt flag
 			__bic_SR_register_on_exit(LPM0_bits);							// Exit active CPU
 			break;
@@ -103,10 +101,10 @@ void __attribute__ ((interrupt(USCI_B0_VECTOR))) USCI_B0_ISR (void)
 
 		case 10:															// Vector 10: RXIFG
 			if (USCI_I2C_ctr_RX == 0) {
-				UCB0CTL1 |= UCTXSTP;                    					// I2C stop condition
+				UCB0CTL1 |= UCTXSTP;										// I2C stop condition
 				*USCI_I2C_ptr_RX = UCB0RXBUF;								// Load data from buffer
 				USCI_I2C_ptr_RX++;											// Increment data pointer
-				__bic_SR_register_on_exit(LPM0_bits);   					// Exit active CPU
+				__bic_SR_register_on_exit(LPM0_bits);						// Exit active CPU
 			}
 			else {
 				*USCI_I2C_ptr_RX = UCB0RXBUF;								// Load data from buffer
@@ -117,9 +115,9 @@ void __attribute__ ((interrupt(USCI_B0_VECTOR))) USCI_B0_ISR (void)
 
 		case 12:															// Vector 12: TXIFG
 			if (USCI_I2C_ctr_TX == 0) {
-				UCB0CTL1 |= UCTXSTP;                    					// I2C stop condition
+				UCB0CTL1 |= UCTXSTP;										// I2C stop condition
 				UCB0IFG &= ~UCTXIFG;										// Clear USCI_B0 TX interrupt flag
-				__bic_SR_register_on_exit(LPM0_bits);   					// Exit active CPU
+				__bic_SR_register_on_exit(LPM0_bits);						// Exit active CPU
 			}
 			else {
 				UCB0TXBUF = *USCI_I2C_ptr_TX;								// Load TX buffer
@@ -131,4 +129,3 @@ void __attribute__ ((interrupt(USCI_B0_VECTOR))) USCI_B0_ISR (void)
 		default: break;
 	}
 }
-
